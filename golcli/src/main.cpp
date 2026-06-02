7887663a45dfcab3b99d4689452da573d789ec14
ptumulty <ptumulty1@gmail.com>
2026-06-01 21:53:16 -0700

    Add ncurses

--- golcli/src/main.cpp
+++ golcli/src/main.cpp
@@ -1,8 +1,22 @@
 
-#include <iostream>
+
+#include <ncurses.h>
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    initscr();
+    noecho();
+    curs_set(0);
+
+    int rows, cols;
+    getmaxyx(stdscr, rows, cols);
+
+    const char *msg = "Hello, World!";
+    mvprintw(rows / 2, (cols - 13) / 2, "%s", msg);
+
+    refresh();
+    getch();
+
+    endwin();
     return 0;
 }
